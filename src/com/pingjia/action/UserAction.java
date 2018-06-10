9cba985b7d9b1b5274feed82f91101a4f6926ecb
similing4 <similing4@163.com>
2018-06-10 20:10:20 +0800

    Update UserAction.java

--- src/com/pingjia/action/UserAction.java
+++ src/com/pingjia/action/UserAction.java
@@ -9,6 +9,7 @@ public class UserAction extends BaseAction {
 	private static final long serialVersionUID = -291901314554100442L;
 
 	public String login() {
+		//只有页面的直接返回SUCCESS就行了。
 		return SUCCESS;
 	}
 	public String reg() {
@@ -19,19 +20,19 @@ public class UserAction extends BaseAction {
 		return success("注销成功","User/login");
 	}
 	public String checkLogin() {
-		DB_Where where = new DB_Where();
+		DB_Where where = new DB_Where();//DB_Where对象用于select的条件处理
 		where.setWhere("username", p("username"));
 		where.setWhere("password", p("password"));
-		List<?> lists = M("User").where(where).select();
-		if(lists.size()==0){
-			return error("用户名或密码错误");
+		List<?> lists = M("User").where(where).select();//设置where条件后执行查询操作
+		if(lists.size()==0){//没查到
+			return error("用户名或密码错误");//返回错误页，前端会跳转到错误页并显示错误信息跳转到提交到本页面的页面
 		}else{
-			User u = (User)lists.get(0);
-			session("UD",u);
+			User u = (User)lists.get(0);//取出用户
+			session("UD",u);//设置session
 			if(u.getPower()==1)
-				return success("登录成功","Index/index");
+				return success("登录成功","Index/index");//跳转到Index命名空间下的index方法
 			else
-				return success("登录成功","Home/index");
+				return success("登录成功","Home/index");//跳转到Home命名空间下的index方法
 		}
 	}
 	public String checkReg() {
@@ -41,11 +42,11 @@ public class UserAction extends BaseAction {
 		if(lists.size()!=0){
 			return error("用户名已存在");
 		}else{
-			User user = new User();
-			user.setUsername(p("username"));
+			User user = new User();//新建Hibernate实体对象
+			user.setUsername(p("username"));//设置参数
 			user.setPassword(p("password"));
 			user.setPower(0);
-			if(M("User").add(user))
+			if(M("User").add(user))//直接M(类名).add方法添加就行了
 				return success("注册成功","User/login");
 			else
 				return error("注册失败","User/reg");
@@ -53,7 +54,7 @@ public class UserAction extends BaseAction {
 	}
 	@Override
 	public boolean check() {
-		// TODO Auto-generated method stub
+		// 没有权限验证的地方直接返回true。
 		return true;
 	}
 }
